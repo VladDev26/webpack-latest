4b52ccf50740922b021dea7662c19ff57cdab808
VladDev26 <volodimirb23@gmail.com>
2017-03-28 21:46:25 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports = {	
+	entry: path.resolve(__dirname, 'src', 'index'),
+	output: {
+		path: path.join(__dirname, 'dist'),
+		// publicPath: 'dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: 'file-loader?name=[name].[ext]'
+			},
+			{ 
+				test: /\.scss$/i, 
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						'css-loader?-url', //disables url inline loading
+						'sass-loader'
+					]
+				})
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('styles/bundle.css')
+	]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const path = require('path');
+
+
+// module.exports = {
+// 	entry: './src/index.js',
+// 	output: {
+// 		path: path.join(__dirname, '/dist'),
+// 		publicPath: '/dist',
+// 		filename: 'scripts/bundle.js'
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.png$/,
+// 				use: [
+// 					'file-loader?name=/[name].[ext]'
+// 				]
+// 			}
+// 			// {
+// 			// 	//extensions (\?.*$|$) with versions
+// 			// 	test: /\.(png|woff|woff2|ept|ttf|svg)(\?.*$|$)/,
+// 			// 	use: [
+// 			// 		'file-loader?name=/[name].[ext]'
+// 			// 	]
+// 			// }
+
+// 			//url-loader limit 100000 kilobytes
+// 		]
+// 	}
+// };
+
+
+
+
+
+
